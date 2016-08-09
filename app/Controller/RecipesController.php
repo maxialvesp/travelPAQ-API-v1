d71da8c3bc4e1779d935accf253a3cadfcb022cf
Facundo Gonzalez <gonzalezfj@unsj-cuim.edu.ar>
2016-08-09 14:54:22 -0300

    token devuelto

--- app/Controller/RecipesController.php
+++ app/Controller/RecipesController.php
@@ -3,8 +3,8 @@ class RecipesController extends AppController {
 
 	public function index() {
 		$this->set(array(
-            'recipes' => ['mmm' => 12, 'jj' => 11],
-            '_serialize' => array('recipes')
+            'token' => $this->decoded_token,
+            '_serialize' => array('token')
         ));
 	}
 
